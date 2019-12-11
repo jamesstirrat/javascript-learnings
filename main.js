ec03be84c34a10b72894ffa95ec744c9bd012113
Jamie Stirrat <46400752+jamiestirrat@users.noreply.github.com>
2019-12-11 08:41:48 +0000

    Functions in JS
    Creating functions, adding parameters and information, getting information

---
+++ main.js
@@ -0,0 +1,12 @@
+function sayHello(greeting) {
+    var response = prompt("What is your name?");
+    alert(greeting + ", " + response + "!");
+}
+sayHello("GM");
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(42, 42));
+console.log(add(43, 43));
